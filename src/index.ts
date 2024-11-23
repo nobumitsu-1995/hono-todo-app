4178cc2ece3e24a5acb14de939a30ae5cc3aad23
nobumitsu_masugata <ys.training220208@gmail.com>
2024-11-23 14:14:08 +0900

    feat: crud処理の追加

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,68 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import prisma from './database.js'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/todos', async (c) => {
+  const data = await prisma.todo.findMany()
+  return c.json({
+    ok: true,
+    data
+  })
+})
+
+app.get('/todos/:id', async (c) => {
+  const id = Number(c.req.param('id'))
+  const data = await prisma.todo.findFirst({
+    where: { id }
+  })
+  return c.json({
+    ok: true,
+    data
+  })
+})
+
+app.patch('/todos/:id', async (c) => {
+  const id = Number(c.req.param('id'))
+  const body = await c.req.json()
+  const data = await prisma.todo.update({
+    where: { id },
+    data: {
+      title: body.title,
+    }
+  })
+  c.status(201)
+  return c.json({
+    ok: true,
+    data
+  })
+})
+
+app.post('/todos', async (c) => {
+  const body = await c.req.json()
+  const data = await prisma.todo.create({
+    data: {
+      title: body.title,
+      status: body.status
+    }
+  })
+  c.status(201)
+  return c.json({
+    ok: true,
+    data
+  })
+})
+
+app.delete('/todos/:id', async (c) => {
+  const id = Number(c.req.param('id'))
+  await prisma.todo.delete({
+    where: { id }
+  })
+  c.status(204)
+  return c.json({
+    ok: true
+  })
 })
 
 const port = 3000
